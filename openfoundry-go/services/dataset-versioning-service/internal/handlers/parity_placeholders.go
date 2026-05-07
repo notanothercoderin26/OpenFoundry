0dab8840347f1a4b5582958d1c3b69ee4345a13d
unnamedlab <unnamedlab@proton.me>
2026-05-07 18:38:32 +0200

    port dataset catalog metadata parity to go

--- openfoundry-go/services/dataset-versioning-service/internal/handlers/parity_placeholders.go
+++ openfoundry-go/services/dataset-versioning-service/internal/handlers/parity_placeholders.go
@@ -9,16 +9,6 @@ func (h *Handlers) notImplemented(w http.ResponseWriter, _ *http.Request, featur
 	})
 }
 
-func (h *Handlers) GetCatalogFacets(w http.ResponseWriter, r *http.Request) {
-	// TODO(dataset-versioning parity): port catalog facet aggregation from Rust data_asset_catalog.
-	h.notImplemented(w, r, "catalog facets")
-}
-
-func (h *Handlers) GetDatasetMetadata(w http.ResponseWriter, r *http.Request) {
-	// TODO(dataset-versioning parity): port internal dataset metadata lookup.
-	h.notImplemented(w, r, "dataset metadata")
-}
-
 func (h *Handlers) CompareViews(w http.ResponseWriter, r *http.Request) {
 	// TODO(dataset-versioning parity): port dataset view comparison.
 	h.notImplemented(w, r, "compare views")
